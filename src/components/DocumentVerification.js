80665a0e2a3d45c6da6333e31053cb06ed19a4a9
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-09-24 22:19:22 +0700

    GREEN: Add file input and generate hash button to DocumentVerification component

--- src/components/DocumentVerification.js
+++ src/components/DocumentVerification.js
@@ -4,6 +4,9 @@ const DocumentVerification = () => {
   return (
     <div className="document-verification">
       <h1 className="title">Document Verification</h1>
+      <label htmlFor="fileInput">Choose a file:</label>
+      <input id="fileInput" type="file" />
+      <button>Generate Hash</button>
     </div>
   );
 };
