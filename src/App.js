792c22d6c788aac96fdaca7974cad33bcf9da297
“Muhammad <sakhran.t@gmail.com>
2024-09-26 11:54:34 +0700

    [RED] Added new View Organisation Page (Try Out) on Main Page, Added Tests for Rendering

--- src/App.js
+++ src/App.js
@@ -7,32 +7,45 @@ const Home = lazy(() => import('./components/Home'));
 const About = lazy(() => import('./components/About'));
 const Contact = lazy(() => import('./components/Contact'));
 const DocumentVerification = lazy(() => import('./components/DocumentVerification'));
+const ViewOrganisation = lazy( () => import('./components/viewOrganisation'));
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
+
           <Route index element={
             <Suspense fallback={<LoadingIndicator />}>
               <Home />
             </Suspense>
           } />
+
           <Route path="about" element={
             <Suspense fallback={<LoadingIndicator />}>
               <About />
             </Suspense>
           } />
+
           <Route path="contact" element={
             <Suspense fallback={<LoadingIndicator />}>
               <Contact />
             </Suspense>
           } />
+
           <Route path="verify" element={
             <Suspense fallback={<LoadingIndicator />}>
               <DocumentVerification />
             </Suspense>
           } />
+
+          <Route path="viewOrganisation" element={
+            <Suspense fallback={<LoadingIndicator />}>
+              <ViewOrganisation />
+            </Suspense>
+          } />
+
+
         </Route>
       </Routes>
     </Router>
--- src/components/__tests__/DocumentVerification.test.js
+++ src/components/__tests__/DocumentVerification.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import DocumentVerification from '../components/DocumentVerification';
+import DocumentVerification from '../DocumentVerification';
 
 test('renders DocumentVerification component', () => {
   render(<DocumentVerification />);
