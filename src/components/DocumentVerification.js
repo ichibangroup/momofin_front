c4f9ffc108359707708810eefcb9bd41fbf167e5
Sirered <gikurniawan.1995@gmail.com>
2024-10-03 15:14:46 +0700

    [FIX] fixing login and DocumentVerification tests

--- src/components/DocumentVerification.js
+++ src/components/DocumentVerification.js
@@ -82,7 +82,7 @@ const DocumentVerification = () => {
     const MAX_FILE_SIZE_IN_MB = 5
     const MAX_FILE_SIZE = MAX_FILE_SIZE_IN_MB * 1024 * 1024;
     if (selectedFile && selectedFile.size > MAX_FILE_SIZE) {
-      setError('File size must be less than ' +MAX_FILE_SIZE+'MB.');
+      setError('File size must be less than ' +MAX_FILE_SIZE_IN_MB+'MB.');
       setFile(null);
       setSubmissionResult(null);
       setVerificationResult(null);
