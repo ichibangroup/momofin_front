a3474fcbd182d5244cdf74cb3fec45ce2d54be1e
“Muhammad <sakhran.t@gmail.com>
2024-10-10 20:25:10 +0700

    [FIX] Fixing More Tests

---
+++ src/components/__tests__/AddUserOrgAdmin.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import AddUserOrgAdmin from '../AddUserOrgAdmin';
+
+describe('AddUserOrgAdmin', () => {
+  it('renders with correct title', () => {
+    render(<AddUserOrgAdmin />);
+    expect(screen.getByText('Add User (Momofin Admin)')).toBeInTheDocument();
+  });
+});
