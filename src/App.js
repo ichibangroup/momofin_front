2f3941f30ad2a37ac65ff7fa4897fe3a075447dd
mariagrizelda <mariahelga423@gmail.com>
2024-10-20 12:24:38 +0700

    [GREEN] Implement ViewOrganisations component and style with CSS to enhance layout and visual design

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ const AddUserOrgAdmin = lazy(() => import('./components/AddUserOrgAdmin'));
 const AddUserMomofinAdmin = lazy(() => import('./components/AddUserMomofinAdmin'));
 const ViewOrganisationUsers = lazy(() => import('./components/ViewOrganisationUsers'));
 const AddNewOrganisation = lazy(() => import('./components/AddNewOrganisation'));
+const ViewOrg = lazy(() => import('./components/ViewOrg'));
+
 
 
 
@@ -47,6 +49,7 @@ function App() {
           <Route path="addUserMomofinAdmin" element={<Suspense fallback={<LoadingIndicator />}><AddUserMomofinAdmin /></Suspense>} />
           <Route path="configOrganisation/viewOrganisationUsers" element={<Suspense fallback={<LoadingIndicator />}><ViewOrganisationUsers /></Suspense>} />
           <Route path="momofinDashboard/addNewOrganisation" element={<Suspense fallback={<LoadingIndicator />}><AddNewOrganisation /></Suspense>} />
+          <Route path="viewOrg" element={<Suspense fallback={<LoadingIndicator />}><ViewOrg /></Suspense>} />
         </Route>
 
         {/* Auth layout routes */}
