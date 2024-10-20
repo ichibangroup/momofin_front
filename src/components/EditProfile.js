05bef46787aa3205715f57564fe86250612996fb
Sirered <gikurniawan.1995@gmail.com>
2024-10-20 13:13:50 +0700

    [REFACTOR] updating dependency arrays to fix netlify deployment 2

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -50,7 +50,7 @@ const EditProfile = () => {
 
   useEffect(() => {
     fetchUserData();
-  }, [fetchUserData]);
+  });
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
