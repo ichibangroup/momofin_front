8b16dc506c62701bface930814d570fecf803094
“Muhammad <sakhran.t@gmail.com>
2024-10-24 15:48:28 +0700

    [RED] Added RBAC Admin User Permissions Functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Home.css';  
 import { useNavigate } from 'react-router-dom';
-
+import api from '../utils/api.js'; // Import API instance
 
 function Home() {
   const navigate = useNavigate();
+  const [userRoles, setUserRoles] = useState([]); // State to store roles array
+
+  // Function to fetch user info
+  const fetchUserInfo = async () => {
+    try {
+      const response = await api.get('/auth/info'); // Call the API
+      setUserRoles(response.data.roles); // Set the roles from response
+    } catch (error) {
+      console.error('Error fetching user info:', error); // Handle error
+    }
+  };
+
+  useEffect(() => {
+    fetchUserInfo(); // Fetch user info on component mount
+  }, []);
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
@@ -17,17 +32,29 @@ function Home() {
       <div className="branding">
         <h1>MOMOFIN</h1>
         <p>A safer place to store your documents.</p>
+        <p>User Roles: {userRoles.join(', ')}</p> {/* Display roles */}
       </div>
       
       <div className="action-boxes-container">
-        <div className="action-box view-box" onClick={() => navigate('viewDocuments')} role="button" onKeyDown={handleKeyDown}style={{ cursor: 'pointer' }}>
+        <div 
+          className="action-box view-box" 
+          onClick={() => navigate('viewDocuments')} 
+          role="button" 
+          onKeyDown={handleKeyDown} 
+          style={{ cursor: 'pointer' }}
+        >
           <h3>View Documents</h3>
         </div>
 
-        <div className="action-box verify-box" onClick={() => navigate('verify')}  role="button" onKeyDown={handleKeyDown} style={{ cursor: 'pointer' }}>
+        <div 
+          className="action-box verify-box" 
+          onClick={() => navigate('verify')}  
+          role="button" 
+          onKeyDown={handleKeyDown} 
+          style={{ cursor: 'pointer' }}
+        >
           <h3>Upload and Verify Documents</h3>
         </div>
-
       </div>
     </div>
   );
