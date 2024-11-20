e577e6c62ba3f1d399ae704cfaa3f522a7f5939d
Pascal Pahlevi <PascalPahlevi@gmail.com>
2024-11-20 22:02:12 +0700

    [GREEN] Completed Page Redesign

--- src/components/AddNewOrganisation.js
+++ src/components/AddNewOrganisation.js
@@ -8,13 +8,22 @@ const AddOrganisation = () => {
   const [industry, setIndustry] = useState('');
   const [address, setAddress] = useState('');
   const [description, setDescription] = useState('');
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
   const [apiError, setApiError] = useState(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isOrgDetailsSubmitted, setIsOrgDetailsSubmitted] = useState(false);
+
   const navigate = useNavigate();
 
-  const handleSubmit = async (event) => {
+  const handleOrgDetailsSubmit = async (event) => {
+    event.preventDefault();
+    setIsOrgDetailsSubmitted(true);
+  };
+
+  const handleAdminDetailsSubmit = async (event) => {
     event.preventDefault();
     setIsSubmitting(true);
     setApiError(null);
@@ -28,12 +37,10 @@ const AddOrganisation = () => {
       adminPassword: password,
     };
 
-    // Implement logic to submit organization and admin user data
     try {
       const response = await api.post('/api/momofin-admin/organizations', organisationData);
       console.log('Submitted organization and admin data: ', response.data);
       if (response.status === 200) {
-        // Clear form after submit
         setName('');
         setIndustry('');
         setAddress('');
@@ -44,92 +51,132 @@ const AddOrganisation = () => {
         navigate('/app/viewOrg');
       }
     } catch (error) {
-      setApiError(error.response?.data?.message || 'Failed to add organisation. Plesae try again.');
+      setApiError(error.response?.data?.message || 'Failed to add organisation. Please try again.');
     } finally {
       setIsSubmitting(false);
     }
   };
 
+  const handleGoBack = () => {
+    setIsOrgDetailsSubmitted(false);
+
+    setUsername('');
+    setPassword('');
+  };
+
+  const handleCancel = () => {
+    // Reset form data and navigate away (or reset to initial state)
+    setName('');
+    setIndustry('');
+    setAddress('');
+    setDescription('');
+    setUsername('');
+    setPassword('');
+
+    // Navigate to a different page (e.g., back to the main dashboard)
+    navigate('/app/viewOrg'); // Or any other route
+  };
+
   return (
-    <div className="container mt-5">
-      <h1 className="text-3xl font-bold">Add Organisation</h1>
-      <div className="card">
-        <div className="card-body">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">Organisation Name:</label>
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="industry" className="form-label">Industry:</label>
-              <input
-                type="text"
-                className="form-control"
-                id="industry"
-                value={industry}
-                onChange={(e) => setIndustry(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="address" className="form-label">Address:</label>
-              <input
-                type="text"
-                className="form-control"
-                id="address"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="description" className="form-label">Description:</label>
-              <textarea
-                className="form-control"
-                id="description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                required
-              />
-            </div>
-            <h2 className="mt-4">Admin User Details</h2>
-            <div className="mb-3">
-              <label htmlFor="username" className="form-label">Username:</label>
-              <input
-                type="text"
-                className="form-control"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form-label">Password:</label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            {apiError && <div className="error-message">{apiError}</div>}
-            <button type="submit" className="btn btn-primary" disabled={isSubmitting}>
-              {isSubmitting ? 'Submitting...' : 'Add Organisation'}
-            </button>
-          </form>
+      <div className="add-organisation-container mt-5">
+        <h1 className="add-organisation-text-3xl font-bold">
+          {isOrgDetailsSubmitted ? 'Input Admin Details' : 'Add Organisation'}
+        </h1>
+        <div className="add-organisation-card">
+          <div className="add-organisation-card-body">
+            {!isOrgDetailsSubmitted ? (
+                <form onSubmit={handleOrgDetailsSubmit}>
+                  <div className="mb-3">
+                    <label htmlFor="name" className="add-organisation-form-label">Organisation Name:</label>
+                    <input
+                        type="text"
+                        className="add-organisation-form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="industry" className="add-organisation-form-label">Industry:</label>
+                    <input
+                        type="text"
+                        className="add-organisation-form-control"
+                        id="industry"
+                        value={industry}
+                        onChange={(e) => setIndustry(e.target.value)}
+                        required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="address" className="add-organisation-form-label">Address:</label>
+                    <input
+                        type="text"
+                        className="add-organisation-form-control"
+                        id="address"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="description" className="add-organisation-form-label">Description:</label>
+                    <textarea
+                        className="add-organisation-form-control"
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
+                    />
+                  </div>
+                  <div className="add-organisation-buttons">
+                    <button type="button" className="add-organisation-cancel-btn" onClick={handleCancel}>
+                      Cancel
+                    </button>
+                    <button type="submit" className="add-organisation-btn continue-btn">
+                      Continue
+                    </button>
+                  </div>
+                </form>
+            ) : (
+                <form onSubmit={handleAdminDetailsSubmit}>
+                  <div className="mb-3">
+                  <label htmlFor="username" className="add-organisation-form-label">Username:</label>
+                    <input
+                        type="text"
+                        className="add-organisation-form-control"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="password" className="add-organisation-form-label">Password:</label>
+                    <input
+                        type="password"
+                        className="add-organisation-form-control"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                  </div>
+                  {apiError && <div className="add-organisation-error-message">{apiError}</div>}
+                  {/* Button container */}
+                  <div className="add-organisation-buttons">
+                    <button type="button" className="add-organisation-back-btn" onClick={handleGoBack}>
+                      Back
+                    </button>
+                    <button type="submit" className="add-organisation-btn submit-btn" disabled={isSubmitting}>
+                      {isSubmitting ? 'Submitting...' : 'Add Organisation'}
+                    </button>
+                  </div>
+                </form>
+            )}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
