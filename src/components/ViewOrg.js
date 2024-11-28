1de5f332e8dee2c91e7ec0666f0a89c99ef59773
Sirered <gikurniawan.1995@gmail.com>
2024-11-28 18:06:58 +0700

    [REFACTOR] Solving Netlify deployment issues part 3

--- src/components/ViewOrg.js
+++ src/components/ViewOrg.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useCallback} from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
   faPencilAlt, 
